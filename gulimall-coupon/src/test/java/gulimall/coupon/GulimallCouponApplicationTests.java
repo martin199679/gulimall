b3eb85dd2c329fdefe00316300c0fd5cf58f3b04
martin199679 <574812239@qq.com>
2022-02-16 23:29:06 +0800

    项目初始化结构

--- gulimall-coupon/src/test/java/gulimall/coupon/GulimallCouponApplicationTests.java
+++ gulimall-coupon/src/test/java/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -1,6 +1,7 @@
 package gulimall.coupon;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
--- gulimall-product/src/test/java/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,32 @@
 package gulimall.product;
 
-import org.junit.jupiter.api.Test;
+import gulimall.product.entity.BrandEntity;
+import gulimall.product.service.BrandService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GulimallProductApplicationTests {
+public class GulimallProductApplicationTests {
+
+    @Autowired
+    private BrandService brandService;
+
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        List<BrandEntity> list = brandService.list();
+        list.forEach(item-> System.out.println(item));
+//        for (BrandEntity brandEntity : list) {
+//            System.out.println(brandEntity);
+//        }
     }
+
+
 
 }
